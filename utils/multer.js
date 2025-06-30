c55f1fd9a47f2bee96881646fd1abb23997ccb96
Divyesh1692 <divyeshvariya1692@gmail.com>
2025-06-30 16:07:53 +0530

    30/6/25

--- utils/multer.js
+++ utils/multer.js
@@ -2,18 +2,6 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary");
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "smartflow360", // your folder in Cloudinary
-    allowed_formats: ["jpg", "png", "jpeg", "pdf", "docx"], // allowed file types
-    resource_type: "raw",
-    type: "upload", //
-    use_filename: true,
-    unique_filename: false,
-  },
-});
-
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage });
 
 module.exports = upload;
