14c29109e4b885629ca7f03690a166208d0eff54
Divyesh1692 <divyeshvariya1692@gmail.com>
2025-09-23 19:51:24 +0530

    bom and mi controller changes 23/9/25

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.use("/api/indiamart", indiamartRoutes);
 
 app.use("/api/health", healthRoutes);
 
-// startScheduler(process.env.INDIAMART_API_KEY);
+startScheduler(process.env.INDIAMART_API_KEY);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
