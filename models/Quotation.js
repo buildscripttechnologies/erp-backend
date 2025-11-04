018b68860db2c9caf3036b22a37a71d9be830e2b
Divyesh1692 <divyeshvariya1692@gmail.com>
2025-11-04 19:54:43 +0530

    add bulk quotation

--- models/Quotation.js
+++ models/Quotation.js
@@ -9,197 +9,210 @@ const quotationSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Customer",
     },
-    orderQty: {
-      type: Number,
-    },
-    productName: {
-      type: String,
-    },
-    sampleNo: {
-      type: String,
-    },
+    date: { type: Date },
     qNo: {
       type: String,
     },
-    prodNo: {
-      type: String,
-    },
-    invoiceNo: {
-      type: String,
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    productionDate: {
-      type: Date,
-    },
-    deliveryDate: {
-      type: Date,
-    },
-    height: {
-      type: Number,
-      default: 0,
-    },
-    width: {
-      type: Number,
-      default: 0,
-    },
-    depth: {
-      type: Number,
-      default: 0,
-    },
-    qty: {
-      type: Number,
-      default: 0,
-    },
-    stitching: {
-      type: Number,
-      default: 0,
-    },
-    printing: {
-      type: Number,
-      default: 0,
-    },
-    others: {
-      type: Number,
-      default: 0,
-    },
+    quotations: [
+      {
+        orderQty: {
+          type: Number,
+        },
+        productName: {
+          type: String,
+        },
+        description: {
+          type: String,
+          default: "",
+        },
+        sampleNo: {
+          type: String,
+        },
 
-    // Rate and Totals
-    unitRate: {
-      type: Number,
-      default: 0,
-    },
-    unitB2BRate: {
-      type: Number,
-      default: 0,
-    },
-    unitD2CRate: {
-      type: Number,
-      default: 0,
-    },
-    totalRate: {
-      type: Number,
-      default: 0,
-    },
-    totalB2BRate: {
-      type: Number,
-      default: 0,
-    },
-    totalD2CRate: {
-      type: Number,
-      default: 0,
-    },
+        prodNo: {
+          type: String,
+        },
+        invoiceNo: {
+          type: String,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+        productionDate: {
+          type: Date,
+        },
+        deliveryDate: {
+          type: Date,
+        },
+        height: {
+          type: Number,
+          default: 0,
+        },
+        width: {
+          type: Number,
+          default: 0,
+        },
+        depth: {
+          type: Number,
+          default: 0,
+        },
+        qty: {
+          type: Number,
+          default: 0,
+        },
+        stitching: {
+          type: Number,
+          default: 0,
+        },
+        printing: {
+          type: Number,
+          default: 0,
+        },
+        others: {
+          type: Number,
+          default: 0,
+        },
 
-    // Percentages
-    B2B: {
-      type: Number,
-      default: 0,
-    },
-    D2C: {
-      type: Number,
-      default: 0,
-    },
-    rejection: {
-      type: Number,
-      default: 0,
-    },
-    QC: {
-      type: Number,
-      default: 0,
-    },
-    machineMaintainance: {
-      type: Number,
-      default: 0,
-    },
-    materialHandling: {
-      type: Number,
-      default: 0,
-    },
-    packaging: {
-      type: Number,
-      default: 0,
-    },
-    shipping: {
-      type: Number,
-      default: 0,
-    },
-    companyOverHead: {
-      type: Number,
-      default: 0,
-    },
-    indirectExpense: {
-      type: Number,
-      default: 0,
-    },
+        // Rate and Totals
+        unitRate: {
+          type: Number,
+          default: 0,
+        },
+        unitB2BRate: {
+          type: Number,
+          default: 0,
+        },
+        unitD2CRate: {
+          type: Number,
+          default: 0,
+        },
+        totalRate: {
+          type: Number,
+          default: 0,
+        },
+        totalB2BRate: {
+          type: Number,
+          default: 0,
+        },
+        totalD2CRate: {
+          type: Number,
+          default: 0,
+        },
 
+        // Percentages
+        B2B: {
+          type: Number,
+          default: 0,
+        },
+        D2C: {
+          type: Number,
+          default: 0,
+        },
+        rejection: {
+          type: Number,
+          default: 0,
+        },
+        QC: {
+          type: Number,
+          default: 0,
+        },
+        machineMaintainance: {
+          type: Number,
+          default: 0,
+        },
+        materialHandling: {
+          type: Number,
+          default: 0,
+        },
+        packaging: {
+          type: Number,
+          default: 0,
+        },
+        shipping: {
+          type: Number,
+          default: 0,
+        },
+        companyOverHead: {
+          type: Number,
+          default: 0,
+        },
+        indirectExpense: {
+          type: Number,
+          default: 0,
+        },
+
+        createdBy: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        productDetails: [
+          {
+            itemId: {
+              type: mongoose.Schema.Types.ObjectId,
+              refPath: "productDetails.type",
+            },
+            type: {
+              type: String,
+              enum: ["RawMaterial", "SFG", "FG"],
+            },
+            partName: String,
+            height: Number,
+            width: Number,
+            // depth: Number,
+            category: String,
+            qty: {
+              type: Number,
+            },
+            grams: {
+              type: Number,
+            },
+            rate: Number,
+            sqInchRate: Number,
+            baseQty: Number,
+            itemRate: Number,
+            cuttingType: String,
+            isPrint: Boolean,
+            isPasting: Boolean,
+          },
+        ],
+        consumptionTable: [
+          {
+            skuCode: String,
+            itemName: String,
+            category: String,
+            weight: { type: String }, // in kg if applicable
+            qty: { type: String }, // in meters, pcs, etc.
+          },
+        ],
+
+        status: {
+          type: String,
+          default: "Pending",
+        },
+
+        isActive: {
+          type: Boolean,
+          default: true,
+        },
+        file: [
+          {
+            fileName: String,
+            fileUrl: String,
+          },
+        ],
+        printingFile: [
+          {
+            fileName: String,
+            fileUrl: String,
+          },
+        ],
+      },
+    ],
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    productDetails: [
-      {
-        itemId: {
-          type: mongoose.Schema.Types.ObjectId,
-          refPath: "productDetails.type",
-        },
-        type: {
-          type: String,
-          enum: ["RawMaterial", "SFG", "FG"],
-        },
-        partName: String,
-        height: Number,
-        width: Number,
-        // depth: Number,
-        category: String,
-        qty: {
-          type: Number,
-        },
-        grams: {
-          type: Number,
-        },
-        rate: Number,
-        sqInchRate: Number,
-        baseQty: Number,
-        itemRate: Number,
-        cuttingType: String,
-        isPrint: Boolean,
-        isPasting: Boolean,
-      },
-    ],
-    consumptionTable: [
-      {
-        skuCode: String,
-        itemName: String,
-        category: String,
-        weight: { type: String }, // in kg if applicable
-        qty: { type: String }, // in meters, pcs, etc.
-      },
-    ],
-
-    status: {
-      type: String,
-      default: "Pending",
-    },
-
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
-    file: [
-      {
-        fileName: String,
-        fileUrl: String,
-      },
-    ],
-    printingFile: [
-      {
-        fileName: String,
-        fileUrl: String,
-      },
-    ],
-
     deletedAt: { type: Date, default: null },
   },
   { timestamps: true }
